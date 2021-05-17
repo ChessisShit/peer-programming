e0ee9e1c705fee7efdf5a5c58322398c9accaf2f
Andrew-McCall <amccall@qa.com>
2021-05-17 11:39:37 +0100

    AM: goldilocks Tests and public method

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,9 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-		System.out.println(dogo (5));
+		for (int item: goldilocks(100, 80, new int[]{10,20,30,100,200}, new int[]{10,20,30,50,200})) {
+			System.out.println(item);
+		}
 
 	}
 	
@@ -24,32 +26,39 @@ public class Runner {
 		
 	}
 	
-	
 	public static String dogo(int input) {
 		String output = "";
-		for (int x = 1; x <= 100; x++)
-		{	
-		if (x != input) {
-			
-			if (x % 10 == 2) {
-			
-				output += x + "nd, ";
-						
-			} else if (x % 10 == 3)  {
-				
-				output += x + "rd, ";
-				
-				
-			} else {
-				output += x + "th, ";
-			}
-			
-			
-			
-		}
-					
+		for (int x = 1; x <= 100; x++){	
+			if (x != input) {
+				if (x % 10 == 2) {
+					output += x + "nd, ";	
+				} else if (x % 10 == 3)  {
+					output += x + "rd, ";
+				} else {
+					output += x + "th, ";
+				}
+			}		
 		}
 	return output.substring(0, output.length()-2);
+	}
+	
+	public static int[] goldilocks(int weight, int temp, int[] chairs, int[] bowls) {
+		
+		int[] output = new int[0];
+		
+		for (int x = 0; x < chairs.length; x++) {
+			if (chairs[x] >= weight && bowls[x] <= temp) {
+				
+				int[] oldList = output;
+				output = new int[output.length+1];
+				for (int y = 0; y < oldList.length; y++) {
+					output[y] = oldList[y]; 
+				}
+				output[output.length-1] = x+1;
+				
+			}
+		}
+		return output;
 	}
 	
 }
--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -13,8 +13,12 @@ public class JunitTest {
 	@Test
 	public void dogoTest() {
 		assertEquals(Runner.dogo(5), "1th, 2nd, 3rd, 4th, 6th, 7th, 8th, 9th, 10th, 11th, 12nd, 13rd, 14th, 15th, 16th, 17th, 18th, 19th, 20th, 21th, 22nd, 23rd, 24th, 25th, 26th, 27th, 28th, 29th, 30th, 31th, 32nd, 33rd, 34th, 35th, 36th, 37th, 38th, 39th, 40th, 41th, 42nd, 43rd, 44th, 45th, 46th, 47th, 48th, 49th, 50th, 51th, 52nd, 53rd, 54th, 55th, 56th, 57th, 58th, 59th, 60th, 61th, 62nd, 63rd, 64th, 65th, 66th, 67th, 68th, 69th, 70th, 71th, 72nd, 73rd, 74th, 75th, 76th, 77th, 78th, 79th, 80th, 81th, 82nd, 83rd, 84th, 85th, 86th, 87th, 88th, 89th, 90th, 91th, 92nd, 93rd, 94th, 95th, 96th, 97th, 98th, 99th, 100th");
-		
-			
+	}
+	
+	@Test
+	public void goldilocksTest() {
+		assertEquals(Runner.goldilocks(100, 80, new int[]{10,20,30,100,200}, new int[]{10,20,30,50,200})[0] , 4);
+		assertEquals(Runner.goldilocks(100, 80, new int[]{10,20,30,50,200}, new int[]{10,20,30,50,200}).length , 0);
 	}
 	
 }
