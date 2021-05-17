655a4daa5d31fa180b194604084a6775117af8ee
Andrew-McCall <amccall@qa.com>
2021-05-17 10:58:29 +0100

    AM: Created testing for revserseFactorial

--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -1,12 +1,12 @@
-
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class JunitTest {
 
+	@Test
 	public void reverseFactorialTest() {
 		assertEquals(Runner.reverseFactorial(120), "5!");
-		assertEquals(Runner.reverseFactorial(1), "1!");
 		assertEquals(Runner.reverseFactorial(150), "NONE");
 	}
 	
